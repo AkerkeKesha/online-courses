ac7cf0d9a1da84e9d3e40e54b214b05cc4de1dbd
Akerke <akerke.kass@gmail.com>
2019-01-05 22:47:25 +0600

    REST API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import 'bootstrap';
 import style from './main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+
+
+$.get('http://127.0.0.1:8000/courses/')
+    .done( function(data) {
+        const courseList = data;
+        const titlesFromBackend = courseList.map( c => c.title); 
+        const descFromBackend = courseList.map ( d => d.description);
+        Array.from(document.getElementsByClassName('course-title')).forEach( 
+            t => t.innerHTML = titlesFromBackend.pop() 
+        );
+        Array.from(document.getElementsByClassName('course-description')).forEach( 
+            d => d.innerHTML = descFromBackend.pop() 
+        );
+
+    })
 
 Array.from(document.getElementsByClassName('collapsible')).forEach(
     collapsible => collapsible.addEventListener('click', () => {
